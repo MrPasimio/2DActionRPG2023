4590e0235faae11209fd58505341a1dca47a0ffb
nightmarejfoxb <118293178+nightmarejfoxb@users.noreply.github.com>
2023-03-20 10:18:10 -0700

    got done with 7

--- Assets/Jacob/codes/Player/Playercr.cs
+++ Assets/Jacob/codes/Player/Playercr.cs
@@ -10,12 +10,25 @@ public class Playercr : MonoBehaviour
     [Header("Components")]
     [SerializeField] private Rigidbody2D rig;
     [SerializeField] private SpriteRenderer sr;
+    [SerializeField] private MouseControl mouseUtilities;
 
     private Vector2 moveInput;
+
+    private void Update()
+    {
+        Vector2 mouseDirection = mouseUtilities.GetMouseDirection(transform.position);
+
+        sr.flipX = mouseDirection.x < 0;
+    }
 
     private void FixedUpdate()
     {
         Vector2 velocity = moveInput * moveSpeed;
         rig.velocity = velocity;
+    }
+
+    public void OnMoveInput (InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
     }
 }
