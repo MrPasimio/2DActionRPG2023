4929bff3a4968ed6a8cfeb28124a6f84a9dd6771
P-O-T-A-T-O-K-I-N-G <potatodragon09@gmail.com>
2023-03-21 10:13:36 -0700

    Lessons7&8Complete

--- Assets/Ian/Scripts/Player/PlayerController.cs
+++ Assets/Ian/Scripts/Player/PlayerController.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
@@ -9,12 +9,24 @@ public class PlayerController : MonoBehaviour
     [Header("Components")]
     [SerializeField] private Rigidbody2D rig;
     [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private MouseUtilities mouseUtilities;
 
     private Vector2 moveInput;
+
+    private void Update()
+    {
+        Vector2 mouseDirection = mouseUtilities.GetMouseDirection(transform.position);
+
+        spriteRenderer.flipX = mouseDirection.x < 0;
+    }
 
     private void FixedUpdate()
     {
         Vector2 velocity = moveInput * moveSpeed;
         rig.velocity = velocity;
+    }
+    public void OnMoveInput (InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
     }
 }
