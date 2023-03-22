9b241b179e40648a451a887c78a8c87ed7e69457
nightmarejfoxb <118293178+nightmarejfoxb@users.noreply.github.com>
2023-03-22 09:40:49 -0700

    got done with video 15

---
+++ Assets/Jacob/codes/UI/HealthBarUI.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HealthBarUI : MonoBehaviour
+{
+    [SerializeField] private character character;
+    [SerializeField] private TextMeshProUGUI nameText;
+    [SerializeField] private Image healthBarFill;
+
+    void SetNameText(string text)
+    {
+        nameText.text = text;
+    }
+
+    void UpdateHealthBar()
+    {
+        float healthPercent = (float)character.CurHp / (float)character.MaxHp;
+        healthBarFill.fillAmount = healthPercent;
+    }
+}
