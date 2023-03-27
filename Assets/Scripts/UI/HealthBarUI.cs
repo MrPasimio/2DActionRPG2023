f7b89659ac8f1ce9231dcce56ee40006ffb84631
MrPasimio <jpasimio@mvsd320.org>
2023-03-26 20:56:33 -0700

    Lesson 16 - Health Bar 1 Complete

---
+++ Assets/Scripts/UI/HealthBarUI.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//L15 Add Libraries
+using UnityEngine.UI;
+using TMPro;
+
+public class HealthBarUI : MonoBehaviour
+{
+    //L15 variables
+    [SerializeField] private Character character;
+    [SerializeField] private TextMeshProUGUI nameText;
+    [SerializeField] Image healthBarFill;
+
+
+    void SetNameText(string text)
+    {
+        nameText.text = text;
+    }
+
+    void UpdateHealthBar()
+    {
+        float healthPercent = (float)character.curHp / (float)character.maxHp;
+        healthBarFill.fillAmount = healthPercent;
+    }
+}
