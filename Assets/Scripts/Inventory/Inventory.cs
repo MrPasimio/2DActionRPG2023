9bb9d77f11e1294f17fecd36033ff4a0a6fed7dd
wsprouse <118293505+wsprouse@users.noreply.github.com>
2023-03-27 09:30:19 -0700

    Lesson 26 Completed

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -4,15 +4,81 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private ItemData[] starterItems;
+    [SerializeField] private int inventorySize;
+    private ItemSlot[] itemSlots;
+
+    public InventoryUI UI;
+
+    public static Inventory Instance;
+
+    void Awake()
     {
-        
+        if (Instance != null && Instance != this)
+            Destroy(gameObject);
+        else
+            Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start ()
     {
-        
+        itemSlots = new ItemSlot[inventorySize];
+
+        for(int i = 0; i < itemSlots.Length; i++)
+        {
+            itemSlots[i] = new ItemSlot();
+        }
+
+        for(int i = 0; i < starterItems.Length; i++)
+        {
+            AddItem(starterItems[i]);
+        }
+    }
+
+    public void AddItem (ItemData item)
+    {
+
+    }
+
+    public void RemoveItem (ItemData item)
+    {
+
+    }
+
+    public void RemoveItem (ItemSlot slot)
+    {
+
+    }
+
+    ItemSlot FindAvailableItemSlot (ItemData item)
+    {
+        for (int i = 0; i < itemSlots.Length; i++)
+        {
+            if (itemSlots[i].Item == item && itemSlots[i].Quantity < item.MaxStackSize)
+                return itemSlots[i];
+        }
+
+        return null;
+    }
+
+    ItemSlot GetEmptySlot()
+    {
+        for(int i = 0; i < itemSlots.Length; i++)
+        {
+            if (itemSlots[i].Item == null)
+                return itemSlots[i];
+        }
+
+        return null;
+    }
+
+    public void UseItem (ItemSlot slot)
+    {
+
+    }
+
+    public bool HasItem (ItemData item)
+    {
+        return false;
     }
 }
