596301edcd37c0e50e4e7e8b83aba9d6882e7eb2
nightmarejfoxb <118293178+nightmarejfoxb@users.noreply.github.com>
2023-03-28 09:10:14 -0700

    got done with video 26

--- Assets/Jacob/codes/Inventory/ItemSlot.cs
+++ Assets/Jacob/codes/Inventory/ItemSlot.cs
@@ -2,17 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemSlot : MonoBehaviour
+public class ItemSlot 
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public itemsData Item;
+    public int Quantiy;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public static int Length { get; internal set; }
 }
