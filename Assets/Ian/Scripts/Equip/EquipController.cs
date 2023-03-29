2481fe20d79032b2464a0d13289e652d7418b7fe
P-O-T-A-T-O-K-I-N-G <potatodragon09@gmail.com>
2023-03-29 09:34:23 -0700

    Lesson22Complete

--- Assets/Ian/Scripts/Equip/EquipController.cs
+++ Assets/Ian/Scripts/Equip/EquipController.cs
@@ -1,18 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class EquipController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private EquipItem curEquipItem;
+    private GameObject curEquipObject;
+
+    private bool useInput;
+
+    [Header("Components")]
+    [SerializeField] private Transform equipObjectOrigin;
+    [SerializeField] private MouseUtilities mouseUtilities;
+
+    private void Update()
     {
-        
+        Vector2 mouseDir = mouseUtilities.GetMouseDirection(transform.position);
+
+        transform.up = mouseDir;
+
+        if(HasItemEquipped())
+        {
+            if(useInput && EventSystem.current.IsPointerOverGameObject() == false)
+            {
+                curEquipItem.OnUse();
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Equip (ItemData item)
     {
-        
+        if(HasItemEquipped())
+        {
+            UnEquip();
+
+            curEquipObject = Instantiate(item.EquipPrefab, equipObjectOrigin);
+            curEquipItem = curEquipObject.GetComponent<EquipItem>();
+        }
+    }
+
+    public void UnEquip ()
+    {
+        if(curEquipItem != null)
+        {
+            Destroy(curEquipItem);
+        }
+
+        curEquipItem = null;
+    }
+
+    public bool HasItemEquipped ()
+    {
+        return curEquipItem != null;
+    }
+
+    public void OnUseInput (InputAction.CallbackContext context)
+    {
+        if(context.phase == InputActionPhase.Performed)
+        {
+            useInput = true;
+        }
+        if(context.phase == InputActionPhase.Canceled)
+        {
+            useInput = false;
+        }
     }
 }
