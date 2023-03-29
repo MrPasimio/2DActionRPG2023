412f930bbef5362a1eda007b830eadeec1b6955f
wsprouse <118293505+wsprouse@users.noreply.github.com>
2023-03-29 10:27:58 -0700

    Lesson 34 2:26 in

--- Assets/Scripts/Equip/RangedEquipItem.cs
+++ Assets/Scripts/Equip/RangedEquipItem.cs
@@ -15,12 +15,18 @@ public class RangedEquipItem : EquipItem
         if (Time.time - lastAttackTime < i.FireRate)
             return;
 
-        // return if we don't have a projectile in our inventory
+        if(Inventory.Instance.HasItem(i.ProjectileItemData) == false)
+            return;
 
         lastAttackTime = Time.time;
 
         // spawn the projectile
+        i.Fire(muzzle.position, muzzle.rotation, Character.Team.Player);
+
         // remove the projectile from our inventory
+        Inventory.Instance.RemoveItem(i.ProjectileItemData);
+
         // play the sound effect
+        AudioManager.Instance.PlayPlayerSound(shootSFX);
     }
 }
