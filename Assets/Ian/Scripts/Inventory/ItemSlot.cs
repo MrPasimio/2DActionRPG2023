31ca9c44963a3e6edc5577e828b61b36d0b7f277
P-O-T-A-T-O-K-I-N-G <potatodragon09@gmail.com>
2023-04-10 10:28:21 -0700

    Lesson26Incomplete

--- Assets/Ian/Scripts/Inventory/ItemSlot.cs
+++ Assets/Ian/Scripts/Inventory/ItemSlot.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class ItemSlot : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public ItemData Item;
+    public int Quantity;
 }
